86716b17c333f4006fb583915d9660f03c4ac8ea
delof <delofi@mail.ru>
2025-08-05 14:21:26 +0300

    Start

---
+++ src/main/java/com/example/TZRESTFullAPI/TZRestFullApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.TZRESTFullAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TZRestFullApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TZRestFullApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TZRESTFullAPI/TZRestFullApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TZRESTFullAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TZRestFullApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
