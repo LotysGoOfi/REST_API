043ceb893e88d7823641a8a13d0bd921ef029c04
delof <delofi@mail.ru>
2025-08-06 16:51:28 +0300

    create HostelController

---
+++ src/main/java/com/example/TZRESTFullAPI/controllers/HostelController.java
@@ -0,0 +1,31 @@
+﻿package com.example.TZRESTFullAPI.controllers;
+
+import com.example.TZRESTFullAPI.dtos.hotels.HotelResponse;
+import com.example.TZRESTFullAPI.dtos.hotels.HotelSummaryResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/property-view")
+public class HostelController {
+
+    @GetMapping("/hotels")
+    @ResponseStatus(value = HttpStatus.OK)
+    public List<HotelSummaryResponse> findByAll(){
+        return null;
+    }
+
+    @GetMapping("hostels/id={id}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public HotelResponse findById(@PathVariable UUID id){
+        return null;
+    }
+}
---
+++ src/main/java/com/example/TZRESTFullAPI/dtos/hotels/HotelSummaryResponse.java
@@ -0,0 +1,17 @@
+﻿package com.example.TZRESTFullAPI.dtos.hotels;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HotelSummaryResponse {
+    private UUID id;
+    private String name;
+}
